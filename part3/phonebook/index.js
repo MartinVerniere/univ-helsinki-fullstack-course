d16d52f8b5e355a68e9d2e8fa3b21287e0781e89
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-07 18:04:17 -0300

    Completed exercise 3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -68,6 +68,34 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.floor(Math.random() * 10000)
+        : 0;
+    console.log(maxId);
+    return String(maxId + 1);
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number missing'
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person);
+
+    response.json(person);
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
