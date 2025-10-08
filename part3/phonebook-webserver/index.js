6a1305914ff8866aba7eecd887c299ea50fa1d87
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-08 16:32:19 -0300

    Removed cors

--- part3/phonebook-webserver/index.js
+++ part3/phonebook-webserver/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
 
 const app = express();
 
@@ -30,7 +29,6 @@ let persons = [
 app.use(express.json());
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors());
 app.use(express.static('dist'));
 
 app.get('/', (request, response) => {
