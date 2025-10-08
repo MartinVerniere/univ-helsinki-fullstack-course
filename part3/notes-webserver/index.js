7560ce658fa4f6b47c2b33d2e2d16f3bbb4066be
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-08 11:22:36 -0300

    Update index.js

--- part3/notes-webserver/index.js
+++ part3/notes-webserver/index.js
@@ -33,6 +33,10 @@ app.use(requestLogger);
 app.use(cors());
 app.use(express.static('dist'));
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+})
+
 app.get('/api/notes', (request, response) => {
     response.json(notes);
 })
