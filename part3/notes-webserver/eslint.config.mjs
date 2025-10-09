0291a3d9df42e444a8365a9c1c74719cb8f31548
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-09 18:09:28 -0300

    Updated notes app - completed section 3d - "Adding More Style Rules"

--- part3/notes-webserver/eslint.config.mjs
+++ part3/notes-webserver/eslint.config.mjs
@@ -1,6 +1,6 @@
-import globals from 'globals';
-import js from '@eslint/js';
-import stylisticJs from '@stylistic/eslint-plugin-js';
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
 	js.configs.recommended,
@@ -19,6 +19,11 @@ export default [
 			'@stylistic/js/linebreak-style': ['error', 'unix'],
 			'@stylistic/js/quotes': ['error', 'single'],
 			'@stylistic/js/semi': ['error', 'never'],
+			eqeqeq: 'error',
+			'no-trailing-spaces': 'error',
+			'object-curly-spacing': ['error', 'always'],
+			'arrow-spacing': ['error', { before: true, after: true }],
+			'no-console': 'off',
 		},
 	},
 	{
