bbd9c5a3919115b383f034b2e1bedd762e5cc2e0
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-11 11:00:37 -0300

    Completed exercise 4.8: Blog List Tests, step 1

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,18 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
