af459592c511492df41c8260fc000de89ea5fa0c
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-11 10:34:59 -0300

    Updated notes app - completed section 4b - "Refactoring the route responsible for adding a note"

--- part4/notes-webserver/tests/notes_api.test.js
+++ part4/notes-webserver/tests/notes_api.test.js
@@ -4,27 +4,17 @@ const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app')
 const Note = require('../models/note')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
 ///
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
-
 beforeEach(
   async () => {
     await Note.deleteMany({})
-    let noteObject = new Note(initialNotes[0])
+    let noteObject = new Note(helper.initialNotes[0])
     await noteObject.save()
-    noteObject = new Note(initialNotes[1])
+    noteObject = new Note(helper.initialNotes[1])
     await noteObject.save()
   })
 
@@ -44,7 +34,7 @@ after(async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, initialNotes.length)
+  assert.strictEqual(response.body.length, helper.initialNotes.length)
 })
 
 ///
@@ -53,4 +43,39 @@ test('a specific note is within the returned notes', async () => {
 
   const contents = response.body.map(e => e.content)
   assert(contents.includes('HTML is easy'))
+})
+
+///
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+///
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
 })
