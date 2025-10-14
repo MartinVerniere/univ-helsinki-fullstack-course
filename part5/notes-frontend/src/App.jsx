70f39d669ea8076f248498c9f11b543ce8287aa3
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-14 10:38:40 -0300

    Copied folder 'notes-frontend' from part3 to part5
    Copying the one from part 2 gave cors errors, so had to copy the one in part 3 that had further changes to cors detailed in part 3b of the course

---
+++ part5/notes-frontend/src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react'
+import noteService from './services/notes'
+import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    noteService.create(noteObject)
+      .then(newNote => {
+        console.log(newNote);
+        setNotes(notes.concat(newNote));
+        setNewNote('');
+      });
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote)
+      .then(newNote => {
+        setNotes(notes.map(n => n.id !== id ? n : newNote));
+      })
+      .catch(error => {
+        console.log(error);
+        setErrorMessage(`Note '${note.content}' was already removed from server`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter(n => n.id !== id));
+      })
+  }
+
+  useEffect(() => {
+    noteService.getAll()
+      .then(initialNotes => {
+        console.log('promise fulfilled');
+        setNotes(initialNotes);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {errorMessage && <Notification message={errorMessage} /> }
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
