10c2a31495eeb806855147f556a6a4c7020fa748
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-16 16:25:28 -0300

    Updated playwright - completed section 5d - "Writing on the form"

--- part5/notes-frontend/src/components/LoginForm.jsx
+++ part5/notes-frontend/src/components/LoginForm.jsx
@@ -20,19 +20,24 @@ const LoginForm = ({ login }) => {
 
             <form onSubmit={loginUser}>
                 <div>
-                    username
-                    <input
-                        value={username}
-                        onChange={event => setUsername(event.target.value)}
-                    />
+                    <label>
+                        username
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={event => setUsername(event.target.value)}
+                        />
+                    </label>
                 </div>
                 <div>
-                    password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                    />
+                    <label>
+                        password
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                    </label>
                 </div>
                 <button type="submit">login</button>
             </form>
