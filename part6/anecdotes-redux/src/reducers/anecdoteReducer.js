33a8456cf5ecd04540e62d96732d859e0bc9a6ac
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-18 16:44:07 -0300

    Completed exercise 6.5: Anecdotes, step 3

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -50,7 +50,9 @@ const reducer = (state = initialState, action) => {
       const anecdoteId = action.payload.id
       const anecdoteToChange = state.find(anecdote => anecdote.id === anecdoteId)
       const newAnecdote = { ...anecdoteToChange, votes: 1 + anecdoteToChange.votes }
-      return state.map(anecdote => (anecdote.id === anecdoteId ? newAnecdote : anecdote))
+      const orderedAnecdotes = state.map(anecdote => (anecdote.id === anecdoteId ? newAnecdote : anecdote))
+      orderedAnecdotes.sort((firstAnecdote, secondAnecdote) => secondAnecdote.votes - firstAnecdote.votes)
+      return orderedAnecdotes
     }
     default:
       return state
