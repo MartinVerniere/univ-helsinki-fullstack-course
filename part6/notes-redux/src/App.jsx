98f7bcfdc8dc1988aedfa39959a252d5a96b01c9
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-20 17:45:27 -0300

    Updated notes-redux app - completed section 6c - "Initializing the store with data fetched from the server"

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from 'react-redux'
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { useEffect } from 'react'
+import { setNotes } from './reducers/NoteReducer'
+import noteService from './services/notes'
 
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        noteService.getAll().then(notes => dispatch(setNotes(notes)))
+    }, [dispatch])
+
     return (
         <div>
             <ul>
