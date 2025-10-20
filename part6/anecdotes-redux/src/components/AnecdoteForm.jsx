b276a311ff6b4ab86e9000ff7be2267d855a932c
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-20 18:19:03 -0300

    Completed exercise 6.15 Anecdotes and the Backend, step 2

--- part6/anecdotes-redux/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -1,18 +1,20 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { clearNotification, notifyCreation } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const anecdote = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(anecdote))
-        dispatch(notifyCreation(`You created '${anecdote}'`))
+        const newNote = await anecdoteService.createNew(anecdote)
+        dispatch(createAnecdote(newNote))
+        dispatch(notifyCreation(`You created '${newNote.content}'`))
         setTimeout(() => {
-            dispatch(clearNotification())
+            dispatch(clearNotification(null))
         }, 5000)
     }
 
