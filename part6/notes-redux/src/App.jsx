13f0a4d086aa3e032842d28adff1d07f80c3065c
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-20 18:30:05 -0300

    Updated notes-redux app - completed section 6c - "Asynchronous actions and Redux Thunk"

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -3,13 +3,12 @@ import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 import { useEffect } from 'react'
-import { setNotes } from './reducers/NoteReducer'
-import noteService from './services/notes'
+import { initializeNotes } from './reducers/NoteReducer'
 
 const App = () => {
     const dispatch = useDispatch()
     useEffect(() => {
-        noteService.getAll().then(notes => dispatch(setNotes(notes)))
+        dispatch(initializeNotes())
     }, [dispatch])
 
     return (
