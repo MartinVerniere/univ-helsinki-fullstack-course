e472563ee8c608b233c6ddd708e1e26a1da4298b
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-20 09:59:09 -0300

    Updated notes-redux app - completed section 6b - "Store with complex state"

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -2,10 +2,34 @@ import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 
 const App = () => {
+    const filterSelected = (value) => {
+        console.log(value)
+    }
+
     return (
         <div>
             <ul>
                 <NoteForm />
+                <div>
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => filterSelected('ALL')}
+                    />
+                    all
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => filterSelected('IMPORTANT')}
+                    />
+                    important
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => filterSelected('NONIMPORTANT')}
+                    />
+                    nonimportant
+                </div>
                 <Notes />
             </ul>
         </div>
