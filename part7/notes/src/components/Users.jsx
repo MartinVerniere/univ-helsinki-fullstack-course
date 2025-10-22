393524c7a1bbae7f8a1b78f429473a16a35f3088
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-22 10:24:35 -0300

    Updated notes app - completed section 7a - "Parameterized route"

--- part7/notes/src/components/Users.jsx
+++
@@ -1,7 +0,0 @@
-const Users = () => (
-    <div>
-        <h2>Users</h2>
-    </div>
-)
-
-export default Users
--- part7/notes/src/main.jsx
+++ part7/notes/src/main.jsx
@@ -1,17 +1,74 @@
 import ReactDOM from 'react-dom/client'
+import { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link,
+  useParams
 } from 'react-router-dom'
-import Home from './components/Home'
-import Notes from './components/Notes'
-import Users from './components/Users'
+
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+  </div>
+)
+
+const Note = ({ notes }) => {
+  const id = useParams().id
+  const note = notes.find(n => n.id === Number(id))
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
+    </div>
+  )
+}
+
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
+)
+
+const Users = () => (
+  <div>
+    <h2>Users</h2>
+  </div>
+)
 
 const App = () => {
 
   const padding = {
     padding: 5
   }
+
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
 
   return (
     <Router>
@@ -22,7 +79,8 @@ const App = () => {
       </div>
 
       <Routes>
-        <Route path="/notes" element={<Notes />} />
+        <Route path="/notes/:id" element={<Note notes={notes} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
         <Route path="/users" element={<Users />} />
         <Route path="/" element={<Home />} />
       </Routes>
