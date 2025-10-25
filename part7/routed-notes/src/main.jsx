2409d35b86749046555c4d0f4106a7ae75685154
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-25 10:18:45 -0300

    Updated routed-notes app - completed section 7c - "Styled components"

--- part7/routed-notes/src/main.jsx
+++ part7/routed-notes/src/main.jsx
@@ -7,7 +7,37 @@ import {
   Navigate,
   useNavigate
 } from 'react-router-dom'
-import { Alert, AppBar, Button, Container, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Toolbar } from '@mui/material'
+import { Alert, AppBar, Container, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Toolbar } from '@mui/material'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div>
@@ -69,16 +99,14 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          <TextField label="username" />
+          username:
+          <Input />
         </div>
         <div>
-          <TextField label="password" type='password' />
+          password:
+          <Input type='password' />
         </div>
-        <div>
-          <Button variant="contained" color="primary" type="submit">
-            login
-          </Button>
-        </div>
+        <Button type="submit" primary=''>login</Button>
       </form>
     </div>
   )
@@ -127,31 +155,16 @@ const App = () => {
   }
 
   return (
-    <Container>
-      {(message &&
-        <Alert severity="success">
-          {message}
-        </Alert>
-      )}
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            home
-          </Button>
-          <Button color="inherit" component={Link} to="/notes">
-            notes
-          </Button>
-          <Button color="inherit" component={Link} to="/users">
-            users
-          </Button>
-          {user
-            ? <em>{user} logged in</em>
-            : <Button color="inherit" component={Link} to="/login">
-              login
-            </Button>
-          }
-        </Toolbar>
-      </AppBar>
+    <Page>
+      <Navigation>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </Navigation>
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
         <Route path="/notes" element={<Notes notes={notes} />} />
@@ -159,11 +172,11 @@ const App = () => {
         <Route path="/login" element={<Login onLogin={login} />} />
         <Route path="/" element={<Home />} />
       </Routes>
-      <div>
-        <br />
-        <em>Note app, Department of Computer Science 2024</em>
-      </div>
-    </Container>
+
+      <Footer>
+        <em>Note app, Department of Computer Science 2022</em>
+      </Footer>
+    </Page>
   )
 }
 
