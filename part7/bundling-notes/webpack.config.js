10b9e884997d6c867519f2285a60ea0184c1efa8
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-25 10:56:19 -0300

    Updated bundling app - completed section 7d - "Webpack-dev-server"

--- part7/bundling-notes/webpack.config.js
+++ part7/bundling-notes/webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path')
 
-const config = () => {
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: 'main.js'
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    },
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'build'),
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-            ],
-        },
-    }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 }
 
 module.exports = config
