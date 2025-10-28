55bc127a6135644556632bc79d7f09985c214778
MartinVerniere <martincho7verniere@yahoo.com>
2025-10-28 16:40:13 -0300

    executed command npx prettier --write .

--- part7/blog-list-react-query/frontend/src/components/BlogList.jsx
+++ part7/blog-list-react-query/frontend/src/components/BlogList.jsx
@@ -1,19 +1,19 @@
-import Blog from "./Blog";
+import Blog from './Blog'
 
 const BlogList = ({ blogs, likeBlog, user, deleteBlog }) => {
-  return (
-    <div>
-      {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          likeBlog={likeBlog}
-          user={user}
-          deleteBlog={deleteBlog}
-        />
-      ))}
-    </div>
-  );
-};
+	return (
+		<div>
+			{blogs.map((blog) => (
+				<Blog
+					key={blog.id}
+					blog={blog}
+					likeBlog={likeBlog}
+					user={user}
+					deleteBlog={deleteBlog}
+				/>
+			))}
+		</div>
+	)
+}
 
-export default BlogList;
+export default BlogList
