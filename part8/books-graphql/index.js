70ef65ddf35936fcba993319cbe47bd7e1e4861a
MartinVerniere <martincho7verniere@yahoo.com>
2025-11-01 12:01:40 -0300

    Completed exercise 8.6: Adding a book

--- part8/books-graphql/index.js
+++ part8/books-graphql/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
 	{
@@ -119,6 +120,15 @@ const typeDefs = `
 		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
 	}
+
+	type Mutation {
+		addBook(
+			title: String!
+			published: Int!
+			author: String!
+			genres: [String!]!
+		): Book
+	}
 `
 
 const resolvers = {
@@ -142,6 +152,17 @@ const resolvers = {
 		bookCount: (root) => {
 			const authorBooks = books.filter(book => book.author === root.name)
 			return authorBooks.length
+		}
+	},
+	Mutation: {
+		addBook: (root, args) => {
+			if (!authors.find(author => author.name === args.author)) {
+				const newAuthor = { name: args.author, id: uuid() }
+				authors = authors.concat(newAuthor)
+			}
+			const newBook = { ...args, id: uuid() }
+			books = books.concat(newBook)
+			return newBook
 		}
 	}
 }
