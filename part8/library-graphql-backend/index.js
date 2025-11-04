3283cb7bcbaddac359dbc02f9ad2bad3be05fefa
MartinVerniere <martincho7verniere@yahoo.com>
2025-11-04 16:28:25 -0300

    Completed exercise 8.14: Database, part 2

--- part8/library-graphql-backend/index.js
+++ part8/library-graphql-backend/index.js
@@ -1,6 +1,5 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -19,102 +18,6 @@ mongoose.connect(MONGODB_URI)
 	.catch((error) => {
 		console.log('error connection to MongoDB:', error.message)
 	})
-
-let authors = [
-	{
-		name: 'Robert Martin',
-		id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-		born: 1952,
-	},
-	{
-		name: 'Martin Fowler',
-		id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-		born: 1963
-	},
-	{
-		name: 'Fyodor Dostoevsky',
-		id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-		born: 1821
-	},
-	{
-		name: 'Joshua Kerievsky', // birthyear not known
-		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-	},
-	{
-		name: 'Sandi Metz', // birthyear not known
-		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-	},
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-
-let books = [
-	{
-		title: 'Clean Code',
-		published: 2008,
-		author: 'Robert Martin',
-		id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-		genres: ['refactoring']
-	},
-	{
-		title: 'Agile software development',
-		published: 2002,
-		author: 'Robert Martin',
-		id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-		genres: ['agile', 'patterns', 'design']
-	},
-	{
-		title: 'Refactoring, edition 2',
-		published: 2018,
-		author: 'Martin Fowler',
-		id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-		genres: ['refactoring']
-	},
-	{
-		title: 'Refactoring to patterns',
-		published: 2008,
-		author: 'Joshua Kerievsky',
-		id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-		genres: ['refactoring', 'patterns']
-	},
-	{
-		title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-		published: 2012,
-		author: 'Sandi Metz',
-		id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-		genres: ['refactoring', 'design']
-	},
-	{
-		title: 'Crime and punishment',
-		published: 1866,
-		author: 'Fyodor Dostoevsky',
-		id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-		genres: ['classic', 'crime']
-	},
-	{
-		title: 'Demons',
-		published: 1872,
-		author: 'Fyodor Dostoevsky',
-		id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-		genres: ['classic', 'revolution']
-	},
-]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
 	type Author {
@@ -155,45 +58,52 @@ const typeDefs = `
 
 const resolvers = {
 	Query: {
-		bookCount: () => books.length,
-		authorCount: () => authors.length,
-		allBooks: (root, args) => {
-			if (!args.author && !args.genre) return books
+		bookCount: async () => await Book.collection.countDocuments(),
+		authorCount: async () => await Author.collection.countDocuments(),
+		allBooks: async (root, args) => {
+			if (!args.author && !args.genre) return await Book.find({}).populate('author')
 			if (args.author && args.genre) {
-				return books.filter(book =>
-					book.author === args.author
-					&& book.genres.find(genre => genre === args.genre)
-				)
+				const author = await Author.findOne({ name: args.author })
+				if (!author) return []
+				return await Book.find({ author: author._id, genres: args.genre }).populate('author')
 			}
-			if (args.author) return books.filter(book => book.author === args.author)
-			if (args.genre) return books.filter(book => book.genres.find(genre => genre === args.genre))
+			if (args.author) {
+				const author = await Author.findOne({ name: args.author })
+				if (!author) return []
+				return await Book.find({ author: author._id }).populate('author')
+			}
+			if (args.genre) return await Book.find({ genres: args.genre }).populate('author')
 		},
-		allAuthors: () => authors
+		allAuthors: async () => await Author.find({})
 	},
 	Author: {
-		bookCount: (root) => {
-			const authorBooks = books.filter(book => book.author === root.name)
-			return authorBooks.length
-		}
+		bookCount: async (root) => await Book.countDocuments({ author: root._id })
 	},
 	Mutation: {
-		addBook: (root, args) => {
-			if (!authors.find(author => author.name === args.author)) {
-				const newAuthor = { name: args.author, id: uuid() }
-				authors = authors.concat(newAuthor)
+		addBook: async (root, args) => {
+			let author = await Author.findOne({ name: args.author })
+			if (!author) {
+				author = new Author({ name: args.author })
+				await author.save()
 			}
-			const newBook = { ...args, id: uuid() }
-			books = books.concat(newBook)
-			return newBook
-		},
-		editAuthor: (root, args) => {
-			const authorToEdit = authors.find(author => author.name === args.name)
 
+			const newBook = new Book({
+				title: args.title,
+				published: args.published,
+				author: author._id,
+				genres: args.genres
+			})
+			const response = await newBook.save()
+			return response.populate('author')
+		},
+		editAuthor: async (root, args) => {
+			const authorToEdit = await Author.findOne({ name: args.name })
 			if (!authorToEdit) return null
 
-			const updatedAuthor = { ...authorToEdit, born: args.setBornTo }
-			authors = authors.map(author => author.name === updatedAuthor.name ? updatedAuthor : author)
-			return updatedAuthor
+			authorToEdit.born = args.setBornTo
+			const response = await authorToEdit.save()
+
+			return response
 		}
 	}
 }
