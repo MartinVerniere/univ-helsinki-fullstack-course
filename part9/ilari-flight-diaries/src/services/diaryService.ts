efc1d2ed90b7979ee28d3666533a8fe9cc4cd0d5
MartinVerniere <mn.verniere@gmail.com>
2025-11-13 15:34:20 -0300

    Updated ilari-flight-diaries app - completed section 6c - "Utility Types"

--- part9/ilari-flight-diaries/src/services/diaryService.ts
+++ part9/ilari-flight-diaries/src/services/diaryService.ts
@@ -1,9 +1,13 @@
 import diaries from '../../data/entries';
 
-import { DiaryEntry } from '../types';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
 	return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+	return diaries.map(({ id, date, weather, visibility }) => ({ id, date, weather, visibility }));
 };
 
 const addDiary = () => {
@@ -12,5 +16,6 @@ const addDiary = () => {
 
 export default {
 	getEntries,
+	getNonSensitiveEntries,
 	addDiary
 };
