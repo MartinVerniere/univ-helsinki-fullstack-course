99e0030ded396486ae2a716a54a776880345d731
MartinVerniere <mn.verniere@gmail.com>
2025-11-13 15:25:07 -0300

    Updated ilari-flight-diaries app - completed section 9c - "Implementing the functionality"

--- part9/ilari-flight-diaries/src/index.ts
+++ part9/ilari-flight-diaries/src/index.ts
@@ -1,3 +1,4 @@
+import diaryRouter from './routes/diaries';
 import express from 'express';
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ app.get('/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
