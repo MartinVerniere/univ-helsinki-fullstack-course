db34d6f679cfdf9d7cbe3e1a0b7fcbab143c31e5
MartinVerniere <mn.verniere@gmail.com>
2025-11-14 09:09:04 -0300

    Updated ilari-flight-diaries app - completed ection 9c - "Adding a new diary"

--- part9/ilari-flight-diaries/src/services/diaryService.ts
+++ part9/ilari-flight-diaries/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaries from '../../data/entries';
 
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry, Visibility, Weather } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
 	return diaries;
@@ -15,8 +15,14 @@ const findById = (id: number): DiaryEntry | undefined => {
 	return entry;
 };
 
-const addDiary = () => {
-	return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+	const newDiaryEntry = {
+		id: Math.max(...diaries.map(d => d.id)) + 1,
+		...entry
+	};
+
+	diaries.push(newDiaryEntry);
+	return newDiaryEntry;
 };
 
 export default {
