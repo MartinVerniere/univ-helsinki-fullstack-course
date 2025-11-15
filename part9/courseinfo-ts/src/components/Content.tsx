39197b1984344f332485c7c6b73f2b61d0837998
MartinVerniere <mn.verniere@gmail.com>
2025-11-15 10:40:36 -0300

    Completed exercise Exercise 9.16

--- part9/courseinfo-ts/src/components/Content.tsx
+++ part9/courseinfo-ts/src/components/Content.tsx
@@ -3,8 +3,11 @@ interface CoursePartBase {
 	exerciseCount: number;
 }
 
-interface CoursePartBasic extends CoursePartBase {
+interface CoursePartDescripted extends CoursePartBase {
 	description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescripted {
 	kind: "basic"
 }
 
@@ -13,26 +16,90 @@ interface CoursePartGroup extends CoursePartBase {
 	kind: "group"
 }
 
-interface CoursePartBackground extends CoursePartBase {
-	description: string;
+interface CoursePartBackground extends CoursePartDescripted {
 	backgroundMaterial: string;
 	kind: "background"
 }
 
-export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+interface CoursePartSpecial extends CoursePartDescripted {
+	requirements: string[];
+	kind: "special"
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
 
 interface ContentProps {
 	courseParts: Array<CoursePart>
 }
 
-const CourseInfo = (props: CoursePart) => <p> {props.name} {props.exerciseCount} </p>
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+	throw new Error(
+		`Unhandled discriminated union member: ${JSON.stringify(value)}`
+	);
+};
+
+const CourseInfoBasic = (props: CoursePartBasic) => {
+	return (
+		<div>
+			<b> {props.name} {props.exerciseCount} </b>
+			<p> {props.description} </p>
+		</div>
+	);
+}
+
+const CourseInfoGroup = (props: CoursePartGroup) => {
+	return (
+		<div>
+			<b> {props.name} {props.exerciseCount} </b>
+			<p> project exercises {props.groupProjectCount} </p>
+		</div>
+	);
+}
+
+const CourseInfoBackground = (props: CoursePartBackground) => {
+	return (
+		<div>
+			<b> {props.name} {props.exerciseCount} </b>
+			<p> {props.description} </p>
+			<p> background material {props.backgroundMaterial} </p>
+		</div>
+	);
+}
+
+const CourseInfoSpecial = (props: CoursePartSpecial) => {
+	return (
+		<div>
+			<b> {props.name} {props.exerciseCount} </b>
+			<p> {props.description} </p>
+			<p> background material {props.requirements} </p>
+		</div>
+	);
+}
 
 const Content = (props: ContentProps) => {
 	return (
 		<div>
-			{props.courseParts.map(coursePart => <CourseInfo {...coursePart} />)}
+			{props.courseParts.map(coursePart => {
+				switch (coursePart.kind) {
+					case "basic":
+						return <CourseInfoBasic {...coursePart} />;
+					case "group":
+						return <CourseInfoGroup {...coursePart} />;
+					case "background":
+						return <CourseInfoBackground {...coursePart} />;
+					case "special":
+						return <CourseInfoSpecial {...coursePart} />
+					default:
+						return assertNever(coursePart);
+				}
+			})}
 		</div>
 	);
-}
+};
+
+
 
 export default Content;
