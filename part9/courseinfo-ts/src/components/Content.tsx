56528d1563493b2742b4982abafdd938c5f5da0f
MartinVerniere <mn.verniere@gmail.com>
2025-11-15 09:46:35 -0300

    Updated courseinfo-ts app - completed section 9d - "Deeper type usage"

--- part9/courseinfo-ts/src/components/Content.tsx
+++ part9/courseinfo-ts/src/components/Content.tsx
@@ -1,18 +1,36 @@
-interface CourseProps {
-	name: string,
-	exerciseCount: number,
+interface CoursePartBase {
+	name: string;
+	exerciseCount: number;
 }
+
+interface CoursePartBasic extends CoursePartBase {
+	description: string;
+	kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+	groupProjectCount: number;
+	kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartBase {
+	description: string;
+	backgroundMaterial: string;
+	kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
 
 interface ContentProps {
-	courses: CourseProps[]
+	courseParts: CoursePart[]
 }
 
-const CourseInfo = (props: CourseProps) => <p> {props.name} {props.exerciseCount} </p>
+const CourseInfo = (props: CoursePart) => <p> {props.name} {props.exerciseCount} </p>
 
 const Content = (props: ContentProps) => {
 	return (
 		<div>
-			{props.courses.map(course => <CourseInfo name={course.name} exerciseCount={course.exerciseCount} />)}
+			{props.courseParts.map(coursePart => <CourseInfo name={coursePart.name} exerciseCount={coursePart.exerciseCount} />)}
 		</div>
 	);
 }
