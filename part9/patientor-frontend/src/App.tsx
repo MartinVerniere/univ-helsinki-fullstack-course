ff78ff5b66ef8a5da4813b4e0cf0bb71f2d67854
MartinVerniere <mn.verniere@gmail.com>
2025-11-19 10:22:38 -0300

    Completed exercise 9.22: Patientor, step2

--- part9/patientor-frontend/src/App.tsx
+++ part9/patientor-frontend/src/App.tsx
@@ -1,43 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Route, Link, Routes, useMatch } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+import { PatientPage } from "./components/PatientPage";
 
 const App = () => {
-  const [patients, setPatients] = useState<Patient[]>([]);
+	const [patients, setPatients] = useState<Patient[]>([]);
 
-  useEffect(() => {
-    void axios.get<void>(`http://localhost:3000/ping`);
+	useEffect(() => {
+		void axios.get<void>(`http://localhost:3000/ping`);
 
-    const fetchPatientList = async () => {
-      const patients = await patientService.getAll();
-      setPatients(patients);
-    };
-    void fetchPatientList();
-  }, []);
-  
-  return (
-    <div className="App">
-      <Router>
-        <Container>
-          <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
-            Patientor
-          </Typography>
-          <Button component={Link} to="/" variant="contained" color="primary">
-            Home
-          </Button>
-          <Divider hidden />
-          <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-          </Routes>
-        </Container>
-      </Router>
-    </div>
-  );
+		const fetchPatientList = async () => {
+			const patients = await patientService.getAll();
+			setPatients(patients);
+		};
+		void fetchPatientList();
+	}, []);
+
+	const match = useMatch('/patients/:id');
+	const patient = match
+		? patients.find(patient => patient.id === match.params.id)
+		: null;
+
+	return (
+		<div className="App">
+			<Container>
+				<Typography variant="h3" style={{ marginBottom: "0.5em" }}>
+					Patientor
+				</Typography>
+				<Button component={Link} to="/" variant="contained" color="primary">
+					Home
+				</Button>
+				<Divider hidden />
+				<Routes>
+					<Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+					{patient && <Route path={`/patients/${patient.id}`} element={<PatientPage patient={patient} />} />}
+				</Routes>
+			</Container>
+		</div>
+	);
 };
 
 export default App;
---
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,20 @@
+import { Female, Male, Title } from "@mui/icons-material";
+import { Patient } from "../../types";
+import { Box, Typography } from "@mui/material";
+
+interface PatientPageProps {
+	patient: Patient
+}
+
+
+export const PatientPage = ({ patient }: PatientPageProps) => {
+	return (
+		<Box>
+			<Typography variant="h4" component="h1">
+				{patient.name} {patient.gender === "male" ? <Male /> : patient.gender === "female" ? <Female /> : null}
+			</Typography>
+			{patient.ssn && <Typography variant="body1">ssn: {patient.ssn}</Typography>}
+			<Typography variant="body1"> occupation: {patient.occupation} </Typography>
+		</Box>
+	);
+};
