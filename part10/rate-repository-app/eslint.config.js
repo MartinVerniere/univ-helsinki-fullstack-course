d3ca5abd4b921112186ab59c2995e4f25fbaa0a4
MartinVerniere <mn.verniere@gmail.com>
2025-11-25 09:45:46 -0300

    Updated rate-repository-app - completed section 10a - "ESLint"

---
+++ part10/rate-repository-app/eslint.config.js
@@ -0,0 +1,35 @@
+import babelParser from "@babel/eslint-parser";
+import reactPlugin from "eslint-plugin-react";
+import reactNativePlugin from "eslint-plugin-react-native";
+
+export default [
+	{
+		ignores: ["node_modules/**"],
+	},
+
+	{
+		files: ["**/*.js", "**/*.jsx"],
+		languageOptions: {
+			parser: babelParser,
+			parserOptions: {
+				requireConfigFile: false,
+				babelOptions: {
+					presets: ["@babel/preset-react"],
+				},
+			},
+		},
+		plugins: {
+			react: reactPlugin,
+			"react-native": reactNativePlugin,
+		},
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+		rules: {
+			"react/prop-types": "off",
+			"react/react-in-jsx-scope": "off",
+		},
+	},
+];
