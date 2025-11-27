4b77f5e56fb02fc5e6e68f3edfd392343f3d559f
MartinVerniere <mn.verniere@gmail.com>
2025-11-27 10:04:54 -0300

    Updated rate-repository-app - completed section 10b - "Routing"

--- part10/rate-repository-app/src/components/Main.jsx
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -1,11 +1,15 @@
 import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
+import { Route, Routes, Navigate } from 'react-router-native';
 
 const Main = () => {
 	return (
 		<>
 			<AppBar />
-			<RepositoryList />
+			<Routes>
+				<Route path="/" element={<RepositoryList />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
 		</>
 	);
 };
