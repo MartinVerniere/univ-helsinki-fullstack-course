85dc87762779d079428162a0a9a346b04a696560
MartinVerniere <mn.verniere@gmail.com>
2025-12-16 11:08:52 -0300

    Completed section 11.12 Health check

--- part11/full-stack-open-pokedex/app.js
+++ part11/full-stack-open-pokedex/app.js
@@ -17,3 +17,9 @@ app.get("/version", (req, res) => {
 app.get("/health", (req, res) => {
 	res.send("ok")
 })
+
+app.get("/health", (req, res) => {
+	// eslint-disable-next-line no-constant-condition
+	if (true) throw ("error...  ")
+	res.send("ok")
+})
